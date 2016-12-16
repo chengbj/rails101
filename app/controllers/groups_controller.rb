68761784520f11e1f93897872686633c1efdcba1
cheng df <cheng.010021@gmail.com>
2016-12-16 20:21:15 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,6 +16,8 @@ class GroupsController < ApplicationController
 
    def create
      @group = Group.new(group_params)
+     @group.user = current_user
+     
      if @group.save
        redirect_to groups_path
      else
@@ -52,5 +54,5 @@ end
 
 
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create]
 end
