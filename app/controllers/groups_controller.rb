27fa2b1be328df83527fb5de8c8c60f561d46353
cheng df <cheng.010021@gmail.com>
2016-12-16 19:37:28 +0800

    move form to partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,8 +23,7 @@ class GroupsController < ApplicationController
      end
   end
 
-     redirect_to groups_path
-   end
+     
 
   def update
     @group = Group.find(params[:id])
@@ -49,5 +48,4 @@ class GroupsController < ApplicationController
  def group_params
    params.require(:group).permit(:title, :description)
  end
-
 end
